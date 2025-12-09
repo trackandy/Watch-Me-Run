f0a9f49f8c1844f916af4b782c540f3e7ef69620
Andrew <Kent>
2025-12-09 16:51:53 -0500

    Initial Commit

---
+++ Watch Me Run/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Watch Me Run
+//
+//  Created by Andy Kent on 12/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Watch Me Run/Watch_Me_RunApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Me_RunApp.swift
+//  Watch Me Run
+//
+//  Created by Andy Kent on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Me_RunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
